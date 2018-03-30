3b51bf28dd260258331c86d12f656a6ed3454c69
Ishan <contact@ishansharma.com>
2018-03-29 22:05:10 -0500

    Adding initial functions (run from here if something fails)

--- AR Scenekit/ViewController.swift
+++ AR Scenekit/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 import ARKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
     }
 
     override func didReceiveMemoryWarning() {
